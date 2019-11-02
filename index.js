6b21d345061f45b733ebbb7a44f48d62110863a6
oheneba <ohenebakob@hotmail.com>
2019-11-02 12:47:49 +0200

    Ex 3.1 GET endpoint at http://localhost:3001/api/persons for phonebook

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+let persons= [
+    {
+      "name": "Josif Stalin",
+      "number": "125789",
+      "id": 1
+    },
+    {
+      "name": "kobby",
+      "number": "777888999",
+      "id": 2
+    },
+    {
+      "name": "Thiago silva",
+      "number": "484546878",
+      "id": 3
+    },
+    {
+      "name": "jon legend",
+      "number": "2549873",
+      "id": 9
+    },
+    {
+      "name": "oheneba",
+      "number": "154844548",
+      "id": 4
+    }
+]
+
+app.get('/', (request, response)=> {
+    response.send(`<p>The info you want is at /api/persons or at /info</p>`)
+    //response.send(`<p>Request received at ${}</p>`);
+});
+
+app.get('/api/persons', (request, response)=> {
+    response.json(persons);
+});
+
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
