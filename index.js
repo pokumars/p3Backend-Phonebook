75101514afe0f18df20456cc1c3d79e73e700178
oheneba <ohenebakob@hotmail.com>
2019-11-05 14:07:50 +0200

    Ex3.9 added cors to index.js so React app can access it

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
