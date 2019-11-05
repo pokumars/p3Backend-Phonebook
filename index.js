592996bd985fc73f871a62bafc339d8b7462cbdc
oheneba <ohenebakob@hotmail.com>
2019-11-05 15:37:38 +0200

    Procfile and port updated

--- index.js
+++ index.js
@@ -147,6 +147,11 @@ app.post('/api/persons',(request, response) => {
 });
 
 
-const port = 3001;
-app.listen(port);
-console.log(`Server running on port ${port}`);
+//const port = 3001;
+//app.listen(port);
+//console.log(`Server running on port ${port}`);
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
