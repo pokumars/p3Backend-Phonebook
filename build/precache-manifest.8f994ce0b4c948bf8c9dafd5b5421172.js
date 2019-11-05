3b35c48f0c636deae43d2cd549fe421e9c8ad8c7
oheneba <ohenebakob@hotmail.com>
2019-11-05 16:27:35 +0200

    uibuild

---
+++ build/precache-manifest.8f994ce0b4c948bf8c9dafd5b5421172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe433d3abe82ed80e8db697d9301533a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee534cf23423ee3578b",
+    "url": "/static/css/main.26853f99.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "6ee534cf23423ee3578b",
+    "url": "/static/js/main.837b88cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
